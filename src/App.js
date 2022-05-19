71e9e9443068e5be029194c184e27945418132e8
mfarras196 <mfarras196@gmail.com>
2022-05-19 21:44:07 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-
+import ToDoList from "./Components/TodoList";
 
 function App() {
+
+  const items = [
+    {
+     id: 1,
+     text: 'Membuang sampah',
+     completed: false
+   },
+   {
+     id: 2,
+     text: 'Membuat roti',
+     completed: false
+   },
+   {
+     id: 3,
+     text: 'Belajar React',
+     completed: false
+   }
+ ];
+
+ const title = 'Things to do';
+
+
   return (
-    <div className="App"></div>
+    <div className="container">
+      <div className="row">
+        <ToDoList items={items} title={title} />
+      </div>
+    </div>
   );
 }
 
